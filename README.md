6b3cb38556f4b0e245b23acb114000a56e693d11
syifaafra <afra.mumt@gmail.com>
2022-11-10 05:22:38 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# Tugas 7 PBP
+
+Elemen Dasar Flutter
+
+## Stateless widget dan Stateful widget
+
+Stateless Widget dapat diartikan sebagai Widget yang tidak dapat dirubah atau tidak akan pernah berubah. Sedangkan Stateful Widget merupakan widget yang dinamis atau dapat berubah. Berbanding terbalik dengan stateless, stateful widget dapat mengupdate tampilan, merubah warna, menambah jumlah baris dll. Jadi dapat disimpulkan bahwa apapun widget yang dapat berubah maka itulah stateful widget.
+
+## Widget yang digunakan pada Tugas 7
+* Text: menciptakan teks.
+* Row, Column: menciptakan layout yang flexible (seperti flexbox).
+* Container: menciptakan elemen persegi panjang.
+* MaterialApp: menciptakan widget-widget umum yang mengikuti Material Design.
+* Scaffold: menciptakan struktur layout Material Design.
+* StatefulWidget: menciptakan widget yang bisa berubah-ubah statenya.
+* Statelesswidget: mencipatakan widget yang tidak bisa berubah state.
+
+## Fungsi dari setState()
+Fungsi setState() yang akan bertugas untuk memberitahu framework bahwa ada object yang berubah pada State, kemudian akan melakukan build ulang pada Widget tersebut. Kita hanya bisa menggunakan fungsi ini pada StatefulWidget, karena seperti yang telah dijelaskan di atas, Stateless widget tidak dapat dirubah.
+
+## Perbedaan antara ‘const’ dengan ‘final’
+* `final` dan `const` merupakan keyword yang dapat digunakan untuk membuat variabel yang bersifat immutable.
+* Perbedaan utama ada pada inialisasi nilai (pemberian nilai) dari variabelnya, `const` mengharuskan variabel harus dinialisasi pada saat kompilasi, nilai bersifat konstan dan secara langsung / eksplisit sehingga pada saat kompilasi variabel `const` sudah memiliki nilai, sedangkan `final` tidak mengharuskan variabel memiliki nilai secara langsung / eskplisit pada saat kompilasi.
+
+## Cara implementasi
+1. Membuat projek flutter bernama counter_7 dengan command `flutter create counter_7`
+2. Mengubah title pada Material App menjadi `counter_7` serta mengubah title pada MyHomePage menjadi `Program Counter`
+3. Pada template flutter, sudah diberikan button untuk increment, maka ditambahkan button untuk decrement serta fungsi decrement pada class `_MyHomePageState`
+4. Supaya teks indikator berubah menjadi "GANJIL" dengan warna biru ketika counter bernilai ganjil dan berubah menjadi “GENAP” dengan warna merah ketika counter bernilai genap, digunakan if-else pada list widget
+5. Bonus : Memodifikasi tombol decrement supaya hanya muncul saat counter>0 dengan menggunakan `if (_counter > 0)` pada floating action button 
+
+## Referensi
+https://belajarflutter.com/
+https://arbiyanto.medium.com/memahami-perbedaan-stateless-vs-stateful-widget-flutter-e8ea12e46ac
---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Program Counter'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter(){
+     if (_counter == 0) return;
+     setState((){
+       _counter--;
+     });
+   }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      // changes text based on counter
+                      if(_counter % 2 == 0) ...[
+                        const Text(
+                          'GENAP',
+                          style: TextStyle(
+                            color: Colors.red,
+                          )
+                        )
+                      ]
+                      else ...[
+                        const Text(
+                          'GANJIL',
+                          style: TextStyle(
+                            color: Colors.blue,
+                          )
+                        )
+                      ],
+
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Padding(
+        padding: EdgeInsets.only(left: 30),
+        child: Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              if (_counter > 0) ...[ //tombol decrement hanya ditampilkan ketika counter > 0
+                FloatingActionButton(
+                  onPressed: _decrementCounter,
+                  tooltip: 'Decrement',
+                  child: const Icon(Icons.remove),
+                ),
+              ],
+
+              Expanded(child: Container()),
+
+              FloatingActionButton(
+                onPressed: _incrementCounter,
+                tooltip: 'Increment',
+                child: const Icon(Icons.add),
+              ),
+            ]
+        )
+      ),
+    );
+  }
+}
